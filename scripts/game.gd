3a6c06e9e048a4a957636bbf23a658c14d5254bc
Thomas Winterfield <twinterfield68@gmail.com>
2024-11-08 23:46:32 -0500

    first commit

---
+++ scripts/game.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var gem_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawn_gem() # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func spawn_gem() -> void:
+	var new_gem = gem_scene.instantiate()
+	var xpos: float = randf_range(70, 1050)
+	new_gem.on_gem_off_screen.connect(game_over)
+	new_gem.position = Vector2(xpos, -50)
+	add_child(new_gem)
+	
+func game_over() -> void:
+	print("game over")
+	
+func _on_timer_timeout() -> void:
+	spawn_gem()
---
+++ scripts/paddle.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@export var speed: float = 60.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#if Input.is_action_pressed("left"):
+		#position.x += -speed * delta
+	#elif Input.is_action_pressed("right"):
+		#position.x += speed * delta
+	position.x += delta * speed * Input.get_axis("left", "right")
+
+
+func _on_gem_area_entered(area: Area2D) -> void:
+	print(area)
